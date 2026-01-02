29cbc560cb15a5d48d6496c1858ef7de6c098116
Kele Heart <hackerman@MacBookPro.lan>
2026-01-02 10:00:21 -0800

    more changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,20 +7,15 @@ function Hero(){
 
 <div id="about" className="container m-2 google-sans flex wrap space-around align-center">
         <div id="hero" className="container flex column align-center google-sans">
-            <h1>Web Developer</h1>
-            <h3>
-                <span>React | Ruby on Rails | Wordpress | Godot</span> 
-            </h3>
-        
+            <h1>Web Developer</h1>        
             <ul>
                 <li>FullStack(LAMP, ROR, MERN, CLOUD)</li>
-
                 <li>Frontend and Mobile Development Certfication</li>
                 <li>Game Developer in free time</li>
                 <li>Writing and Painting as a hobby</li>
             </ul>
 
-            Currently pursuing additional certifications:
+            <h3>Currently pursuing additional certifications:</h3>
             <ul>
                 <li>Comptia A+</li>
                 <li>Comptia Network+</li>
--- src/components/KHHeader.tsx
+++ src/components/KHHeader.tsx
@@ -1,16 +1,24 @@
-//import { useState } from "react";
+import { useState } from "react";
 
 function KHHeader() {
-  //const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="tiktok-sans">
+    <header className="tiktok-sans bg-frosted">
       <div className="container flex align-center space-between">
         <div id="brand">Kele Heart</div>
-        <nav>
-          <a>About</a>
-          <a>Portfolio</a>
-          <a>Contact</a>
+        <div id="hamburger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+          <span className="menu-line"></span>
+          <span className="menu-line"></span>
+          <span className="menu-line"></span>
+        </div>
+        <nav 
+          style={menuOpen ? { display: "flex" } : { display: "none" }} 
+          className="column align-center bg-frosted column m-1 p-1"
+        >
+          <a href="#about">About</a>
+          <a href="#portfolio">Portfolio</a>
+          <a href="#contact">Contact</a>
         </nav>
       </div>
     </header>
